d2efe689dfdf67b5ccd4bb99f7f7d457a1fa5039
Flemming <flemming.christensen.fc@siemensgamesa.com>
2021-08-05 16:02:33 +0200

    StringCollectionIteration part

---
+++ ScalarTypesOperatorsControlFlow/StringCollectionIteration.py
@@ -0,0 +1,33 @@
+# String
+# 'Flemming' or "Flemming" important is that you are consistance about it
+#
+
+# for newlines Multiline string and Escape sequences like \n
+""" this is
+a multiline
+string"""
+
+'''So 
+is 
+this.'''
+
+path = r'C:\Users\FLEMMING.CHRISTENSE'
+print(path)
+
+s = 'parrot'
+print(s[4])
+
+#type(s[4]) shows it is class 'str' in the python virtual enviroment
+
+# in python consol help(str) shows you all the functions string class have
+
+c = 'oslo'
+print(c)
+# returns a new string called Oslo
+c = c.capitalize()
+print(c)
+
+## bytes
+
+
+
