a5a2ab251d07f0b1f127d9354ed3b9c7c5c90548
Flemming <flemming.christensen.fc@siemensgamesa.com>
2021-08-11 15:59:14 +0200

    Modules in Python
    Execute Modules by passing them as the first argument to Python
    
    All top-level statements are executed when a module is imported
    
    Define functions with the "def" keyword
    
    return without an argument returns "None" as dose the implicit return
    
    Use __name__ to determine how a module is being used
    
    If __name__ == '__main__' lets our module be executalbe and importable
    
    A module is executable once, on first import
    
    def is a statement which binds code to a name
    
    sys.argv contains command line arguments
    
    Dynamic typing supports generic programming
    
    !docstrings :-D

--- PythonModules/words.py
+++ PythonModules/words.py
@@ -1,13 +1,48 @@
+""" Retrieve and print words from a URL."""
+
+import sys
 from urllib.request import urlopen
 
-story = urlopen('http://sixty-north.com/c/t.txt')
-#list
-story_words = []
-for line in story:
-    line_words = line.decode('utf-8').split()
-    for word in line_words:
-        story_words.append(word)
-story.close()
+def fecth_words(url):
+    #""" Python docstrings """"
+    """Fetch a list of words from a URL.
 
-for word in story_words:
-    print(word)
+        Args:
+            url: The URL of a UTF-8 text document.
+
+        Returns:
+            A list of strings containing the words from
+            the document
+    """
+    story = urlopen(url)
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf-8').split()
+        for word in line_words:
+            story_words.append(word)
+    story.close()
+    return story_words
+
+
+def print_items(items):
+    """Print items one per line.
+
+        Args:
+            An iterable series of printalbe items.
+    """
+    for item in items:
+        print(item)
+
+
+def main(url):
+    """Print Each word from a text document from a URL.
+
+            Args:
+                url: The URL of a UTF-8 text document.
+    """
+    words = fecth_words(url)
+    print_items(words)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1]) # The 0th arg is the module filename.
