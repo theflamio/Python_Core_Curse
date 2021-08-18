4514ed51ec95c95da87665dc7e51d05841624ea0
Flemming Christensen <flamekfor@gmail.com>
2021-08-18 20:02:17 +0200

    project files

--- ObjectsAndTypes/FunctionArguments.py
+++ ObjectsAndTypes/FunctionArguments.py
@@ -12,4 +12,32 @@ banner("Denmark is great")
 banner("Denmark is great", ':-D')
 
 #keyword argument border=
-banner(message="Hello world", border='*')
+banner(message="Hello world", border="*")
+
+banner(border="*", message="Hello world")
+
+# Remember that "def" is a statement executed at runtime
+
+# Default arguments are evaluated when def is executed.
+
+# Immutable default values don't cause problems "ALWAYS use immutable objects for default values"
+
+def add_spam(menu=[]):
+    menu.append('spam')
+    return menu
+
+print(add_spam(['Egg', 'beans']))
+print(add_spam())
+print(add_spam())
+
+#Immutable Default Value
+
+def add_spam_Two(menu=None):
+    if menu is None:
+        menu = []
+    menu.append('spam')
+    return menu
+
+print(add_spam_Two())
+print(add_spam_Two())
+print(add_spam_Two())
